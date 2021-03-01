21005c25955f64122d79b562f2c4813ac3530e3a
Bryce <brycezhang.cn@gmail.com>
2021-03-01 14:53:33 +0800

    fix:web不设置自动首页

--- lib/store/middleware.dart
+++ lib/store/middleware.dart
@@ -83,16 +83,12 @@ Middleware<AppState> _verifyAuthState() {
         store.dispatch(LocalUpdateUserAction(user));
         if (kIsWeb) {
         } else {
-          Keys.navigatorKey.currentState
-              .pushReplacementNamed(Routes.shop + '/username1');
-          // Keys.navigatorKey.currentState.pushReplacementNamed(Routes.home);
+          Keys.navigatorKey.currentState.pushReplacementNamed(Routes.home);
         }
       } else {
         store.dispatch(LocalUpdateUserAction(User()));
         store.dispatch(AnonymousLoginAction());
         if (kIsWeb) {
-          Keys.navigatorKey.currentState
-              .pushReplacementNamed(Routes.shop + '/username1');
         } else {
           Keys.navigatorKey.currentState.pushReplacementNamed(Routes.welcome);
         }
