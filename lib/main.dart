73478a6691386a61a9f032e7d7a4434c0ee47581
Bryce <brycezhang.cn@gmail.com>
2021-03-02 16:13:50 +0800

    fix:移除#路径,release下无效

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fans/app.dart';
-import 'package:url_strategy/url_strategy.dart';
+// import 'package:url_strategy/url_strategy.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy();
+  // setPathUrlStrategy();
   runApp(ReduxApp());
 }
